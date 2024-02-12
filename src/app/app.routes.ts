e59e903c040d0283384cf86ad42169a1009f3216
Abhiroop Santra <abhiroop.santra@gmail.com>
2024-02-12 12:41:55 +0400

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // routes for login
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
